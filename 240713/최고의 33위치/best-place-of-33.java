8abeff1fa27eebbb7e147d596bc0a36e43e1bc45
Hee Sang <118061713+codrin2@users.noreply.github.com>
2024-07-13 23:06:20 +0900

    Title: 최고의 33위치, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240713/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N  = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < N-2; i++) {
+            sum = 0;
+            for (int j = 0; j < N-2; j++) {
+                for (int k = i; k < i+3; k++) {
+                    for (int l = j; l < j+3; l++) {
+                        sum += arr[k][l];
+                    }
+                }
+                if(sum > max) {
+                    max = sum;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
